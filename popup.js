0d812e252d9244c7432f06bdd379d1de833fd1e6
Nikhil Bansal <niban@microsoft.com>
2019-07-23 17:23:03 -0700

    refactored code

--- popup.js
+++ popup.js
@@ -7,44 +7,39 @@
 let scrape = document.getElementById('scrape');
 
 
-/*chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});*/
+
 
 scrape.onclick = () => {
 
-    // Get HTML from web-page
-
-    // Extract plain text
-    //var plainText = extractContent("<p>Hello</p><a href='http://w3c.org'>W3C</a>.  Nice to <em>see</em><strong><em>you!</em></strong>", true);
-
-    
-    
-
-    // Create JSON Payload
-    //TODO: Extract URL and Header from webpage
-
-    var jsonPayload = {};
-    getURLandTitle();
-    jsonPayload["url"] = window.localStorage.getItem("uri");
-    jsonPayload["header"] = window.localStorage.getItem("head");
-
-    chrome.runtime.onMessage.addListener(function (msg) {
-        if (msg.text !== undefined) {
-            verify("ddddddddddddddd");
-            var plainText = extractContent(msg.text, true);
-            jsonPayload["body"] = plainText;
-        }
-    });
-
-    // Create an HTTP Request
-    load("http://trustednews.westus.cloudapp.azure.com/api/get_website_score", JSON.stringify(jsonPayload), verify);
-
-    verify(jsonPayload);
+    createRequestAndSend();
 
 };
 
+function createRequestAndSend() {
+    chrome.tabs.query({ 'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT },
+        function (tabs) {
+            var jsonPayload = {};
+            jsonPayload["url"] = tabs[0].url;
+            jsonPayload["header"] = tabs[0].title;
+
+            chrome.tabs.executeScript(tabs[0].id, { code: 'chrome.runtime.sendMessage({text: document.documentElement.innerHTML});' });
+
+
+            chrome.runtime.onMessage.addListener(function (msg) {
+                if (msg.text !== undefined) {
+                    var plainText = extractContent(msg.text, true);
+                    jsonPayload["body"] = plainText;
+                }
+            });
+            load("http://trustednews.westus.cloudapp.azure.com/api/get_website_score", JSON.stringify(jsonPayload), verify);
+        }
+    );
+};
+
+
+function handleResponse(response) {
+
+}
 
 
 function load(url, body, callback) {
@@ -75,17 +70,6 @@ function extractContent(s, space) {
     return [span.textContent || span.innerText].toString().replace(/ +/g, ' ');
 };
 
-//TODO: Merge getURL and getTitle
-function getURLandTitle() {
-    chrome.tabs.query({ 'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT },
-        function (tabs) {
-            window.localStorage.setItem("uri", tabs[0].url);
-            window.localStorage.setItem("head", tabs[0].title);
-            //const scriptToExec = `(${scrapeThePage})()`;
-            chrome.tabs.executeScript(tabs[0].id, { code: 'chrome.runtime.sendMessage({text: document.documentElement.innerHTML});' });
-        }
-    );
-};
 
 
 function verify(jsonPayloadString) {
