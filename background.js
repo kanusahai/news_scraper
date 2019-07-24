e37f7c89a40169bcd5a3ec9705c8978775db7f5b
Kanu Sahai <kanu.sahai@gmail.com>
2019-07-24 13:58:19 -0700

    Refactor and adding relevant popup functionality

--- background.js
+++ background.js
@@ -5,15 +5,77 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+    console.log('Background Script started running');
 });
+
+// Listener for when you load a new web-page
+// Only activates trust-worthiness analysis is the URL contains "news"
+chrome.webNavigation.onCompleted.addListener(function () {
+    console.log('Navigated to a news webpage');
+    calculateTrustScore();
+
+}, {url: [{urlMatches : ".*news.*"}]});
+
+
+function calculateTrustScore() {
+    chrome.tabs.query({ 'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT },
+        function (tabs) {
+            var jsonPayload = {};
+            jsonPayload["url"] = tabs[0].url;
+            jsonPayload["header"] = tabs[0].title;
+            calculateTrustScoreInternal(tabs[0].id, jsonPayload);
+
+            //chrome.tabs.executeScript(tabs[0].id, { code: 'chrome.runtime.sendMessage({text: document.documentElement.innerHTML});' });
+
+            /*chrome.runtime.onMessage.addListener(function (msg) {
+                if (msg.text !== undefined) {
+                    var plainText = stripHTML(msg.text, true);
+                    //jsonPayload["body"] = plainText;
+                    calculateTrustScoreInternal(tabs[0].id, jsonPayload);
+                }
+            }); */        
+        }
+    );
+};
+
+function calculateTrustScoreInternal(tabID, jsonPayload)
+{
+    console.log(jsonPayload);
+    var url = "http://trustednews.westus.cloudapp.azure.com/api/get_website_score";
+    sendRequestToMLModel(tabID, url, JSON.stringify(jsonPayload), handleResponse);
+};
+
+function sendRequestToMLModel(tabID, url, body, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(tabID, xhr.response);
+        }
+    }
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(body);
+};
+
+function handleResponse(tabID, response) {
+    console.log(response);
+
+    //TODO: Parse response here and display the popup accordingly
+    chrome.browserAction.setPopup({tabId : tabID, popup : "popups/popup1.html"});
+};
+
+function stripHTML(str, space) {
+    var span = document.createElement('span');
+
+    span.innerHTML = str;
+    if (space) {
+        var children = span.querySelectorAll('*');
+        for (var i = 0; i < children.length; i++) {
+            if (children[i].textContent)
+                children[i].textContent += ' ';
+            else
+                children[i].innerText += ' ';
+        }
+    }
+    return [span.textContent || span.innerText].toString().replace(/ +/g, ' ');
+};
